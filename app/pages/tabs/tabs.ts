aabfa1870678ddaa3208da4b4c62721a0926ae49
Ewerton Willams <ewertonws@gmail.com>
2016-09-29 00:58:14 -0300

    initial commit

---
+++ app/pages/tabs/tabs.ts
@@ -0,0 +1,23 @@
+import {Page} from 'ionic-angular';
+import { Component } from '@angular/core';
+import { HomePage } from '../home/home';
+import { AboutPage } from '../about/about';
+import { ContactPage } from '../contact/contact';
+
+@Page({
+  templateUrl: 'build/pages/tabs/tabs.html'
+})
+export class TabsPage {
+
+  public tab1Root: any;
+  public tab2Root: any;
+  public tab3Root: any;
+
+  constructor() {
+    // this tells the tabs component which Pages
+    // should be each tab's root Page
+    this.tab1Root = HomePage;
+    this.tab2Root = AboutPage;
+    this.tab3Root = ContactPage;
+  }
+}
